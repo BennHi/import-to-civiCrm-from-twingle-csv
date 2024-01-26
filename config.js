246e4d9c1cec4febf6e85bd718781dcae38b8eba
BennHi <benni-wasmer@freenet.de>
2024-01-26 17:22:41 +0100

    debug on by default

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
     url: 'https://my.website.com', // Host-Adress of your Civi-Site. For example https://my.website.com
     apiEndpoint: '/api/endpoint', // API-Endpoint of your Civi-CRM. eg. /wp-json/civicrm/v3/rest in WordPress. Check with your installation; even your WordPress path might differ.
-    apiKey: '...', // API Key of the Civi-User used to import the Data. Refer to twingle Documentation for further Information.
-    siteKey: '...', // Site Key for your CiviCRM. Refer to twingle Documentation for further Information.
-    debugMode: false, // True will only fill a logfile with the raw API-Calls. Set to false when you want to make actual API calls
+    apiKey: 'your_secret_api_key', // API Key of the Civi-User used to import the Data. Refer to twingle Documentation for further Information.
+    siteKey: 'your_secret_site_key', // Site Key for your CiviCRM. Refer to twingle Documentation for further Information.
+    debugMode: true, // True will only fill a logfile with the raw API-Calls. Set to false when you want to make actual API calls
     csvFilePath: 'path/to/twingle-data.csv', // CSV-File to import from.
     logFilePath: 'path/to/logfile.txt', // Specify the log file path
     projectLookupTable: { // Lookup table to match the project_id from the csv to the project_id for civi in twinglemanager
